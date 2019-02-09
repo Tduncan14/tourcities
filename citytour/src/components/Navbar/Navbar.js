c0a8c3bb5ba079b148ef543a77e4aa251bdd637f
unknown <nbatre1@yahoo.com>
2019-02-08 23:53:21 -0600

    keep moving forward

--- citytour/src/components/Navbar/Navbar.js
+++ citytour/src/components/Navbar/Navbar.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import './Navbar.scss';
+import logo from '../../img/logo.png'
 
 export default function Navbar(){
-    return(
-        <div>
-         Keep Moving forward from navbar
-        </div>
-    )
+    return( <nav className="navbar">
+    <img src= {logo}  alt="logo"/>
+
+    <ul className ="nav-links">
+    <li>
+      <a href="/" className="nav-link">
+           Home
+       </a>
+    </li>
+    <li>
+      <a href="/" className="nav-link">
+           About
+       </a>
+    </li>
+    <li>
+      <a href="/" className="nav-link active">
+           Tours
+       </a>
+    </li>
+    </ul>
+    </nav> )
+
 }
