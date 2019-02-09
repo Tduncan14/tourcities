dcbc48e75c010d7ca2f6d558dbf5f0d6c8440f0b
unknown <nbatre1@yahoo.com>
2019-02-08 21:51:42 -0600

    keep moving forward

--- citytour/src/App.js
+++ citytour/src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import './App.scss';
+import Navbar from './components/Navbar/Navbar'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+       <Navbar />
+
         <h1>hello treek</h1>
       </div>
     );
--- citytour/src/components/Navbar/Navbar.js
+++ citytour/src/components/Navbar/Navbar.js
@@ -4,7 +4,7 @@ import './Navbar.scss';
 export default function Navbar(){
     return(
         <div>
-
+         Keep Moving forward from navbar
         </div>
     )
 }
