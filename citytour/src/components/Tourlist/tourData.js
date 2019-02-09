4f992853944e582e8c71f18fa2288c57315143b2
unknown <nbatre1@yahoo.com>
2019-02-09 16:02:43 -0600

    keep moving forward

--- citytour/src/components/Tourlist/tourData.js
+++ citytour/src/components/Tourlist/tourData.js
@@ -13,6 +13,35 @@ export const tourData = [
         img:'./img/california.jpg',
         name:'Calfornia Tour',
         info:'More real text will come'
+      },
+      {
+          id:3,
+          city:'South Africa',
+          img:'./img/africa.jpg',
+          name:'Africa Tour',
+          info:'More real text will come'
+      },
+      {
+          id:4,
+          city:'France',
+          img:'./img/france.jpg',
+          name:'France tour',
+          info:'More real text will come soon'
+      },
+        {
+          id:5,
+          city:'Greece',
+          img:'./img/greece.jpg',
+          name:'Greece Tour',
+          info:'More real text will come soon'
+        },
 
-      }
+        {
+            id:6,
+            city:'Japan',
+            img:'./img/japan.jpg',
+            name:'Japan Tour',
+            info:'More real text will come soon'
+        }
+
 ]
